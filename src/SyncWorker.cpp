89e01b158c022482b3ce1ef9ea5cc48ad24a9394
Shubo Yang <yangshubo@outlook.com>
2015-11-25 17:54:02 +0800

    adjust some codes

--- src/SyncWorker.cpp
+++ src/SyncWorker.cpp
@@ -2,18 +2,30 @@
 
 NS_MARATON_BEGIN
 
-void SyncWorker::create( const size_t & loop_time , 
-                         const syncworker_callback_t  work_callback , 
-                         const syncworker_after_callback_t  after_callback , 
+void SyncWorker::create( const syncworker_callback_t  work_callback , 
+                         const syncworker_callback_t  after_callback , 
                          void * data )
 {
     SyncWorker* worker      = new SyncWorker();
     worker->cb_work_        = work_callback;
     worker->cb_after_work_  = after_callback;
-    worker->loop_time_      = loop_time;
+    worker->loop_time_      = 1;
     worker->data( data );
     worker->start();
-} 
+}
+
+void SyncWorker::create( const size_t time_span , 
+                         const syncworker_callback_t work_callback , 
+                         const syncworker_callback_t after_callback , 
+                         void * data )
+{
+    SyncWorker* worker      = new SyncWorker();
+    worker->cb_work_        = work_callback;
+    worker->cb_after_work_  = after_callback;
+    worker->loop_time_      = time_span;
+    worker->data( data );
+    worker->start();
+}
 
 void SyncWorker::stop()
 {
@@ -45,15 +57,14 @@ void SyncWorker::uv_process_timer_tick_callback( uv_timer_t * handle )
     ++worker->loop_count_;
 
     if ( worker->finished_ )
-    {
-
-
+    { 
         if ( worker->cb_after_work_ != nullptr )
         {
             worker->cb_after_work_( worker );
         }
 
         int result = uv_timer_stop( &worker->timer_ );
+
         LOG_DEBUG_UV( result );
         
         SAFE_DELETE( worker );

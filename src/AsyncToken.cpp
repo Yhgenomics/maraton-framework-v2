8e480758730ab542885f8e273e8584898d557824
Shubo Yang <yangshubo@outlook.com>
2016-01-12 15:29:48 +0800

    fix compiling errors on linux

--- src/AsyncToken.cpp
+++ src/AsyncToken.cpp
@@ -2,33 +2,6 @@
 
 NS_MARATON_BEGIN
 
-AsyncToken::AsyncToken( callback_t callback )
-{
-    this->callback_     = callback;
-    this->async_.data   = this;
-    uv_async_init( uv_default_loop( ) ,
-                   &this->async_ ,
-                   AsyncToken::uv_async_callback );
-}
 
-AsyncToken::~AsyncToken( )
-{
-    uv_close( ( uv_handle_t* ) &this->async_ , NULL );
-}
-
-void AsyncToken::send( void * data )
-{
-    this->data_ = data;
-    uv_async_send( &this->async_ );
-}
-
-void AsyncToken::uv_async_callback( uv_async_t * handle )
-{
-    AsyncToken* token = scast<AsyncToken*>( handle->data );
-    if ( token->callback_ != nullptr )
-    {
-        token->callback_( token , token->data_ );
-    }
-}
 
 NS_MARATON_END

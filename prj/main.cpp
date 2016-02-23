a1465e4b06700505a96a8ab61fb38ec399acd171
Shubo Yang <yangshubo@outlook.com>
2016-02-23 14:14:14 +0800

    modify on Manager

--- prj/main.cpp
+++ prj/main.cpp
@@ -2,12 +2,18 @@
 //#include <thread>
 //#include <ostream>
 //#include <string>
+//#include <Manager.h>
 //
 //using namespace MRT;
 //using namespace std;
 //
 //int main( )
 //{
+//
+//    MRT::Manager<MRT::Buffer> mgr;
+//
+//    mgr.push( make_sptr( MRT::Buffer ) );
+//
 //    size_t i = SystemInfo::CPUNum( );
 //    size_t j = SystemInfo::CPUSpeed( 1 );
 //    size_t m = SystemInfo::MemorySize( );
